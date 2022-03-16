baf6ba51537dfa3cbeda899360e364177a6f0769
user123145 <shk9510@gmail.com>
2022-03-16 09:48:06 +0900

    수업 03.16

---
+++ src/main/java/Ex03_02.java
@@ -0,0 +1,14 @@
+public class Ex03_02 {
+
+
+    public static void main(String[] args) {
+        System.out.printf("%d", 100+200);
+        System.out.println();
+        System.out.printf("100+100");
+        System.out.println("2342434");
+
+
+
+    }
+
+}
---
+++ src/main/java/Exam01.java
@@ -0,0 +1,16 @@
+public class Exam01 {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("강석현");
+
+
+    }
+
+
+
+
+
+
+}
