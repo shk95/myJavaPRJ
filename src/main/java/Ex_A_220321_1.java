982d33f6c7e42afb8f9eadfda6e1452869955aeb
user123145 <shk9510@gmail.com>
2022-03-21 12:51:27 +0900

    /22.03.21 /개인 복습 /계산문제 입력후 결과값 true false 표시 /작성중.

---
+++ src/main/java/Ex_A_220321_1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Ex_A_220321_1 {
+/*간단 수학계산 테스트
+ex) a * b = c   [a b c 를 입력하고, 계산을 맞춘다.*/
+//1.수식 입력//
+//2.실행후 맞았는지 틀렸는지 판단결과를 출력
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int a,b,c,d,e;
+        //입력숫자
+        boolean A;
+        //계산결과 true fail
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        c=a*b;
+
+
+        System.out.println()
+        //계산결과화면 출력.
+        System.out.println()
+        //맞았다 or 틀렸다.
+    }
+
+
+}
