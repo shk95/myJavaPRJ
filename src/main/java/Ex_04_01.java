ad3c997783b6f274d4cabedfc17c3ef829c60b26
user123145 <shk9510@gmail.com>
2022-03-23 10:11:13 +0900

    /22.03.23 /수업 예제 /응용

--- src/main/java/Ex_04_01.java
+++ src/main/java/Ex_04_01.java
@@ -9,11 +9,11 @@ public class Ex_04_01 {
         System.out.printf(" %d = %d = %d \n", b, c, a);
 
         a = b * c;
-        System.out.printf(" %d / %d = %d \n", b, c, a);
+        System.out.printf(" %d * %d = %d \n", b, c, a);
 
         a = b / c;
-        System.out.printf(" %d / %d = %d \n", b, c, a);
-
+        System.out.printf(" %f / %f = %f \n", (float)b, (float)c, (float)a);
+        //정수형 을 숫자로 변환해봄
         a = b % c;
         System.out.printf(" %d %% %d = %d \n", b, c, a);
 
