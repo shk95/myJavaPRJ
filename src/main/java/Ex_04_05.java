3f71f4cf265e224929b281f46f4a0fd912d37b06
user123145 <shk9510@gmail.com>
2022-03-23 11:29:17 +0900

    /22.03.23 /수업 예제 /

---
+++ src/main/java/Ex_04_05.java
@@ -0,0 +1,16 @@
+public class Ex_04_05 {
+    public static void main(String[] args) {
+        int a = 100, b = 200;
+
+        System.out.printf(" %d == %d 는 %s 이다.\n", a, b, a == b);
+        System.out.printf(" %d != %d 는 %s 이다.\n", a, b, a != b);
+        System.out.printf(" %d > %d 는 %s 이다.\n", a, b, a > b);
+        System.out.printf(" %d < %d 는 %s 이다.\n", a, b, a < b);
+        System.out.printf(" %d >= %d 는 %s 이다.\n", a, b, a >= b);
+        System.out.printf(" %d <= %d 는 %s 이다.\n", a, b, a <= b);
+
+
+        System.out.printf(" %d = %d 는 %s 이다.\n", a, b, a = b);
+        // == 이라는 같다는 표현의 잘못된 예.
+    }
+}
