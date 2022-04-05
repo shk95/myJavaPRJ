d3cbba4addb9e4b55ec105a0012b93d5e624ebb1
user123145 <shk9510@gmail.com>
2022-04-05 15:13:28 +0900

    /22.04.05 /백준 /증감 연산자 활용 구구단 작성

---
+++ src/main/java/Ex_BJ_220405_1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;//백준 구구단
+public class Ex_BJ_220405_1{
+    public static void main(String[]args){
+        Scanner s = new Scanner(System.in);//문자 N 입력
+        int a = 1,b;
+        b = s.nextInt();
+
+        System.out.printf("%d * %d = %d\n",b,a,a*b);//증가연산자 활용
+        System.out.printf("%d * %d = %d\n",b,++a,a*b);
+        System.out.printf("%d * %d = %d\n",b,++a,a*b);
+        System.out.printf("%d * %d = %d\n",b,++a,a*b);
+        System.out.printf("%d * %d = %d\n",b,++a,a*b);
+        System.out.printf("%d * %d = %d\n",b,++a,a*b);
+        System.out.printf("%d * %d = %d\n",b,++a,a*b);
+        System.out.printf("%d * %d = %d\n",b,++a,a*b);
+        System.out.printf("%d * %d = %d\n",b,++a,a*b);
+
+
+    }
+}
