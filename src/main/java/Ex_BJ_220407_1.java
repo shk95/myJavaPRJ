e5c7f6b2eded464ec7ec856f8d59076fbcbfae42
user123145 <shk9510@gmail.com>
2022-04-08 11:42:27 +0900

    /22.04.08 /백준 8393 /문제해결

--- src/main/java/Ex_BJ_220407_1.java
+++ src/main/java/Ex_BJ_220407_1.java
@@ -3,10 +3,10 @@ public class Ex_BJ_220407_1{
     public static void main(String[]args){
         //입력된 값 a 까지 1에서 부터 순차적으로 합산.
         Scanner s = new Scanner(System.in);
-        int a = s.nextInt(), b = 1;//b = 합계
+        int a = s.nextInt(), b = 0;//b = 합계
 
-        for (int i = 0; i < a; i++){//i 에서 입력받은 a 값 까지
-        b+=i;//문제가 있음.
+        for (int i = 1; i <= a; i++){//i 에서 입력받은 a 값 까지
+        b+=i;//문제해결 , 숫자 시작이 잘못됨.
             System.out.printf("%d = b값, %d = i 값\n",b,i);//확인용
         }
         System.out.printf("%d",b);
