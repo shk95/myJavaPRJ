f3b5898780fdce45d3d176a57626ed9a5ded0d4c
user123145 <shk9510@gmail.com>
2022-04-11 16:33:52 +0900

    /22.04.11 /백준 2439 /for 구문 안에서 for 구문을 공백과 * 에 각각 하나씩 씀. /속도면에서 조금 느리다.

---
+++ src/main/java/Ex_BJ_220411_3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ex_BJ_220411_3 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();//출력 층 수
+
+        for (int i = 1; i <= a; i++) {//층수 : 1~입력값 a 층까지
+            for (int k = a; k - i > 0; k--) {//공백 출력
+                System.out.print(" ");
+            }
+            for (int n = 0; n < i; n++) {//*출력
+                System.out.print("*");
+            }
+            System.out.print("\n");//행넘기기
+        }
+    }
+}
