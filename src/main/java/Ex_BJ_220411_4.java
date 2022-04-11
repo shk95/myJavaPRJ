97ec08ddfa49ec352fc2ed7112fc64ebd136762a
user123145 <shk9510@gmail.com>
2022-04-11 16:58:32 +0900

    /22.04.11 /백준 10871 /for 문 안에 if 로 직관적이지만 속도 많이 느림. /속도 개선 필요

---
+++ src/main/java/Ex_BJ_220411_4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ex_BJ_220411_4 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();//수열 을 이루는 정수 개수
+        int X = s.nextInt();//비교할 값
+
+        for (int i = 0; i < N; i++) {//비교를 위해 N 번 반복필요.
+            int x = s.nextInt();//비교대상 값
+            if (X > x) {
+                System.out.printf("%d ", x);
+            }
+        }
+    }
+}
+
+
+
