692758c42a5d4ab4688b04d79c87a9b31b5c7f27
user123145 <shk9510@gmail.com>
2022-04-14 22:40:03 +0900

    /22.04.14 /코드업 1371 /중첩 for 문 학습 /마름모 출력

---
+++ src/main/java/Ex_CU_220414.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Ex_CU_220414{
+    public static void main(String[]args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+//코드업 문제 1371 마름모 출력
+
+        for (int i = 0; i < n; i++){//가로
+            for (int j = i; n-j > 1; j++){
+                System.out.print(" ");//첫번째 공백
+            }
+            System.out.print("*");//첫번째 별
+            for (int k = 0; i*2>k; k++){
+                System.out.print(" ");//두번째 공백
+            }
+            System.out.print("*");//두번째 별
+            System.out.println();//줄변경
+        }
+        for (int i = 0; i < n; i++){//뒤집어서
+            for (int j = 0; j < i; j++){
+                System.out.print(" ");//처음 공백
+            }
+            System.out.print("*");//첫번째 별
+            for (int k = 0; k < (n-i-1)*2; k++){
+                System.out.print(" ");//두번째 공백
+            }
+            System.out.print("*");//두번째 별
+            System.out.println();//줄변경
+        }
+
+    }
+}
