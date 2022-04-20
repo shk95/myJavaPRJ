e7505ad3f8ca1f7744b60abc00d12f2fd572d153
user123145 <shk9510@gmail.com>
2022-04-20 14:27:19 +0900

    /22.04.20 /논리연산자

--- src/main/java/Ex_test_04.java
+++ src/main/java/Ex_test_04.java
@@ -1,6 +1,7 @@
 public class Ex_test_04 {
     public static void main(String[] args) {
         int x = 50;
+//50에서 100까지 더하는데 3,7,11 의 배수는 제외하는 문제.
 
         for (int i = 51; i < 101; i++) {
             if ((i % 3 == 0) || (i % 7 == 0) || (i % 11 == 0)) {
@@ -12,5 +13,25 @@ public class Ex_test_04 {
 
         }
         System.out.println(x);
+
+        x = 50;
+        for (int i = 51; i < 101; i++) {
+            if ((i % 3 != 0) && (i % 7 != 0) && (i % 11 != 0)) {
+                x = x + i;
+            }
+
+        }
+        System.out.println(x);
+
+        x = 50;
+
+        for (int i = 51; i < 101; i++) {
+            if (!((i % 3 == 0) || (i % 7 == 0) || (i % 11 == 0))) {
+                x = x + i;
+            }
+        }
+        System.out.println(x);
     }
 }
+//논리연산 관계 정확히..
+
