cbcc8dc95f24992c56eae5590378f3e4be133e3a
user123145 <shk9510@gmail.com>
2022-04-20 14:35:04 +0900

    /22.04.20 /논리연산자

--- src/main/java/Ex_test_04.java
+++ src/main/java/Ex_test_04.java
@@ -9,7 +9,7 @@ public class Ex_test_04 {
                 // }else{
                 //System.out.println(i);
             }
-            x = x + i;
+            x += i;
 
         }
         System.out.println(x);
@@ -17,7 +17,7 @@ public class Ex_test_04 {
         x = 50;
         for (int i = 51; i < 101; i++) {
             if ((i % 3 != 0) && (i % 7 != 0) && (i % 11 != 0)) {
-                x = x + i;
+                x += i;
             }
 
         }
@@ -27,7 +27,7 @@ public class Ex_test_04 {
 
         for (int i = 51; i < 101; i++) {
             if (!((i % 3 == 0) || (i % 7 == 0) || (i % 11 == 0))) {
-                x = x + i;
+                x += i;
             }
         }
         System.out.println(x);
