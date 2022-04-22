0a9a15fa5ea9218242a0210df7efd722af7725c2
seok-hyeon Kang <shk9510@gmail.com>
2022-04-22 18:23:48 +0900

    Update and rename Ex_BJ_220408.java to Ex_BJ_220408_1.java

--- src/main/java/Ex_BJ_220408_1.java
+++ src/main/java/Ex_BJ_220408_1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;//입력된 값 에서부터 1까지 출력
 
-public class Ex_BJ_220408 {
+public class Ex_BJ_220408_1 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int a = s.nextInt();
