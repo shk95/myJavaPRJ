6955d2e4d5ed59ead946201334df7feed9619eac
user123145 <shk9510@gmail.com>
2022-04-25 14:44:10 +0900

    /22.04.25 /코드업 문제 코드 수정 /2중 for문 함수관계

--- src/main/java/Ex_CU_1365_1_220420.java
+++ src/main/java/Ex_CU_1365_1_220420.java
@@ -4,74 +4,31 @@ import java.util.Scanner;
 public class Ex_CU_1365_1_220420 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        int x = n / 2;//짝수 홀수 관계없이 한번에 받음.
-        for (int i = 0; i < n; i++) {
-            System.out.print("*");
-        }
-        System.out.println();//1행
-        int i = 0;
-        while (i < x - 1) {
-            System.out.print("*");
-            for (int j = 0; j < i; j++) {
-                System.out.print(" ");
-            }
-            System.out.print("*");
-            for (int k = 0; k < x * 2 - 4 - 2 * i; k++) {//가운데 공백
-                System.out.print(" ");
+        int n;
+        n = s.nextInt();
 
-            }
-            if (n % 2 != 0) {//홀수일때
-                System.out.print(" ");
-            }
+        for (int i = 0; i < n; i++) {//row 1 출력
             System.out.print("*");
-            for (int l = 0; l < i; l++) {
-                System.out.print(" ");
-            }
-            System.out.print("*");
-            System.out.println();
-            i++;
         }
-        --i;
-        if (n % 2 != 0) {//홀수일때 가운대 행 추가
-            System.out.print("*");
-            for (int a = 1; a < x; a++) {
-                System.out.print(" ");
+        System.out.println();
+        /*
+        x 자 구조를 함수관계로 생각
+        y = x, y = -x
+        row 2 ~ n-1 까지출력
+        */
+        for (int i = 1; i < n - 1; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((j == 0) || (j == i) || (j == n - 1 - i) || (j == n - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
-            System.out.print("*");
-            for (int a = 1; a < x; a++) {
-                System.out.print(" ");
-            }
-            System.out.print("*");
             System.out.println();
         }
-        //마지막부분 부터 거꾸로 출력.
-        while (i > -1) {
-            System.out.print("*");
-            for (int j = 0; j < i; j++) {
-                System.out.print(" ");
-            }
-            System.out.print("*");
-            for (int k = 0; k < x * 2 - 4 - 2 * i; k++) {//가운데 공백
-                System.out.print(" ");
 
-            }
-            if (n % 2 != 0) {
-                System.out.print(" ");
-            }
+        for (int i = 0; i < n; i++) {//row n 출력
             System.out.print("*");
-            for (int l = 0; l < i; l++) {
-                System.out.print(" ");
-            }
-            System.out.print("*");
-            System.out.println();
-            i--;
-
-        }
-        for (int j = 0; j < n; j++) {
-            System.out.print("*");//마지막 행
-
-
         }
 
 
