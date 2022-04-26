c6d2cd14d419e70828327595db1fcf4ebf241a59
user123145 <shk9510@gmail.com>
2022-04-26 23:02:51 +0900

    /22.04.26 /코드 수정 /2중 for문 함수관계를 활용

--- src/main/java/Ex_CU_1371_1_220414.java
+++ src/main/java/Ex_CU_1371_1_220414.java
@@ -1,32 +1,23 @@
 import java.util.Scanner;
-public class Ex_CU_1371_1_220414{
-    public static void main(String[]args){
+
+public class Ex_CU_1371_1_220414 {
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
 //코드업 문제 1371 마름모 출력
 
-        for (int i = 0; i < n; i++){//가로
-            for (int j = i; n-j > 1; j++){
-                System.out.print(" ");//첫번째 공백
-            }
-            System.out.print("*");//첫번째 별
-            for (int k = 0; i*2>k; k++){
-                System.out.print(" ");//두번째 공백
-            }
-            System.out.print("*");//두번째 별
-            System.out.println();//줄변경
-        }
-        for (int i = 0; i < n; i++){//뒤집어서
-            for (int j = 0; j < i; j++){
-                System.out.print(" ");//처음 공백
-            }
-            System.out.print("*");//첫번째 별
-            for (int k = 0; k < (n-i-1)*2; k++){
-                System.out.print(" ");//두번째 공백
-            }
-            System.out.print("*");//두번째 별
-            System.out.println();//줄변경
-        }
+        for (int x = -n; x < n + 1; x++) {//함수관계로 수정
+            for (int y = -n; y < n + 1; y++) {
+                if ((y == x + n) || (y == x - n) || (y == -x + n) || (y == -x - n)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(".");
 
+                }
+
+            }
+            System.out.println();
+
+        }
     }
 }
