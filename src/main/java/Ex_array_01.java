acd093bfaec909f086f2ac7067b2d6e3fccf3edf
user123145 <shk9510@gmail.com>
2022-04-27 12:20:01 +0900

    /22.04.27 /수업 /배열

---
+++ src/main/java/Ex_array_01.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex_array_01 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        //List<String> Collection 인터페이스 중 List,<> 부분은 String으로 값 입력.
+        //list = 변수명.
+        //ArrayList 는 그중 클래스
+
+        list.add("가나다");
+        list.add("마바사");
+        list.add("아자차");
+        list.add("타파하");
+
+        int listSize = list.size();//size 속성으로 리스트 길이 구하기
+
+        for (int i = 0; i < listSize; i++) {//속도가 가장 느림
+            System.out.println("내용 : " + list.get(i));
+        }
+
+    }
+}
