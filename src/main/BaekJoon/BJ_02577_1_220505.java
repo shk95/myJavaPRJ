72a219ce925dfcd3a4401735d63f24b4847682f8
user123145 <shk9510@gmail.com>
2022-05-05 22:07:53 +0900

    /22.05.05 /백준 /iterator 대신 for문으로 size 구해서 반복. iterator 사용시 값을 가져올때 매번 변수에 값을 저장 해야하는데 이때 매번 저장하는게 무의미한 행위로 인식되는것 같음(작동안함).

--- src/main/BaekJoon/BJ_02577_1_220505.java
+++ src/main/BaekJoon/BJ_02577_1_220505.java
@@ -11,22 +11,21 @@ public class BJ_02577_1_220505 {
         int x = A * B * C;//세 수의 합
         //각자리의 숫자 나누기
         int i = 1;//증감
+        //숫자를 각 자리마다 쪼개서 배열에 하나식 저장
         while (true) {
             int z = (x / i) % 10;
             L.add(z);
             i *= 10;
-            if (x / i == 0) {
+            if (x / i == 0) {//마지막자리 확인
                 break;
             }
         }
-        Iterator<Integer> it = L.iterator();
-        int j;
-        for (j = 0; j < 10; j++) {
-            int k = 0;
-            int a;
-            while (it.hasNext()) {
-                a = it.next();
-                if (a == j) {
+        //자릿수별 숫자 개수
+        for (int j = 0; j < 10; j++) {//0 부터 9까지
+            int k = 0;//숫자 개수
+            int S = L.size();//배열의 크기 = 자리수
+            for (int m = 0; m < S; m++) {
+                if(L.get(m)==j){//j 숫자가 있는지
                     k++;
                 }
             }
