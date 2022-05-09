0516a63ca1f71366aa649c949e9955458d301a68
user123145 <shk9510@gmail.com>
2022-05-09 14:45:40 +0900

    /22.05.09 /백준 3052 /배열을 중복을 제거해서 하나 더 만들기. 문제에 비해서 조금 쓸대없는 부분이 있는듯하다.

--- src/main/BaekJoon/BJ_03052_1_220506.java
+++ src/main/BaekJoon/BJ_03052_1_220506.java
@@ -3,7 +3,10 @@ import java.util.*;
 public class BJ_03052_1_220506 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        List<Integer> L = new ArrayList<>();
+        //특정 숫자 나열을 42(변수 B)로 나눈후 나머지 개수(중복제외) 구하기
+        //중복을 제외하고, 원래 배열의 순서대로 새로운 배열 생성.
+        List<Integer> L = new ArrayList<>();//초기 데이터 입력
+        List<Integer> L2 = new ArrayList<>();//중복 제거된 리스트
 
         int A, B = 42;
 
@@ -11,18 +14,28 @@ public class BJ_03052_1_220506 {
             A = s.nextInt();
             L.add(A % B);
         }
-        int k = 0;
-        //수정중
+        int sizeL = L.size();//L 의 크기값
+        int i = 1;//L 배열의 인덱스
+        int j = 0;//카운터
+        int k = 0;//카운터
 
-        /*for (int i = 0; i < L.size(); i++){
-            int x = L.get(i);
-            for (int j = 0; j < L.size(); j++) {
-                int y = L.get(j);
-                if (x!=y) {
-                    k++;
+        L2.add(L.get(0));//첫번째 값은 중복이 없으므로 가져온다
+
+        do {
+            for (; k < L2.size(); k++) {//중복검사
+                if (Objects.equals(L.get(i), L2.get(k))) {
+                    j++;//중복이 있다면 카운트
                 }
             }
-        }
-        System.out.println(L.size()-k);*/
+            if (j == 0) {//중복이 없을때 기존 L 에서 L2 로 가져온다
+                L2.add(L.get(i));
+            }
+            i++;
+            j = 0;//카운트 초기화
+            k = 0;//카운트 초기화
+        } while (i != sizeL);
+
+        int sizeL2 = L2.size();
+        System.out.print(sizeL2);//중복을 제외한 나머지들의 개수
     }
 }
