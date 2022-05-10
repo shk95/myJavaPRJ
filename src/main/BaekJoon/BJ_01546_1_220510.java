72a6a20d48663f751956b4fde7b1317489b73be0
user123145 <shk9510@gmail.com>
2022-05-10 13:24:26 +0900

    /22.05.10 /백준 1546 /생각보다 시간단축이 안된다. 좀더 알아봐야할듯

--- src/main/BaekJoon/BJ_01546_1_220510.java
+++ src/main/BaekJoon/BJ_01546_1_220510.java
@@ -5,30 +5,16 @@ public class BJ_01546_1_220510 {
         Scanner s = new Scanner(System.in);
         List<Float> list = new ArrayList<>();
 
-        int listSize = s.nextInt();
-        //과목의 개수
+        int listSize = s.nextInt();//과목의 개수
+        float x = 0;//출력값
+        float M = 0;//최댓값
 
         for (int i = 0; i < listSize; i++) {
             list.add(s.nextFloat());
+            x += list.get(i);//하나에 합쳐서 시간 단축
+            M = Math.max(M, list.get(i));//매스 클래스 맥스 메소드
         }
         //과목별 점수들
-
-        float M;//최댓값
-        int j = 0;//최댓값 인덱스
-
-        for (int i = 1; i < listSize; i++) {
-            if (list.get(j) < list.get(i)) {
-                j = i;
-            }
-        }
-
-        M = list.get(j);
-
-        float x = 0;//출력값
-
-        for (int i = 0; i < listSize; i++) {
-            x += list.get(i);
-        }
 
         x = x / M * 100 / listSize;
 
