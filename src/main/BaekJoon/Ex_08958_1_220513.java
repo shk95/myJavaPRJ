f32f0d940bf53953da25fa2ae3e0ec33fbdcfd41
user123145 <shk9510@gmail.com>
2022-05-13 19:09:28 +0900

    /22.05.13 /백준 문제 /문자열 비교 배열

---
+++ src/main/BaekJoon/Ex_08958_1_220513.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ex_08958_1_220513 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int N = s.nextInt();
+        for (int i = 0; i < N; i++) {
+            int n = 0;
+            int k = 1;
+            int sum = 0;
+            String str = s.next();
+            int length = str.length();
+            List<Character> list = new ArrayList<>();
+            do {
+                list.add(str.charAt(n));
+                char c = list.get(n);
+                if (c == 'O') {
+                    sum += k;
+                    k++;
+                } else {
+                    k = 1;
+                }
+                n++;
+            } while (length != n);
+
+            list = null;
+
+            System.out.println(sum);
+        }
+    }
+}
