355a9a0a3e34b96adcd2ce624ae209d69d01ddbc
user123145 <shk9510@gmail.com>
2022-06-06 15:42:34 +0900

    루프를 이용한 방법 추가

--- src/main/etc/Fibonacci.java
+++ src/main/etc/Fibonacci.java
@@ -3,20 +3,41 @@ import java.util.Scanner;
 public class Fibonacci {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int num = s.nextInt()+1;
+        int num = s.nextInt() + 1;
         System.out.print("피보나치수열\n");
         for (int i = 1; i < num; i++) {
-            int x = function(i);
-            System.out.print(i+"항 : "+x+"\n");
+            int x = Fibonacci_recursive(i);
+            System.out.print(i + "항 : " + x + "\n");
+        }
+        System.out.print("피보나치수열\n");
+        for (int i = 1; i < num; i++) {
+            int x = Fibonacci_loop(i);
+            System.out.print(i + "항 : " + x + "\n");
         }
     }
-    public static int function(int n) {
-        if(n<=2){
+
+    public static int Fibonacci_recursive(int n) {
+        if (n <= 2) {
             return 1;
-        }else{
+        } else {
             //재귀적 함수사용.
-            return function(n-1)+function(n-2);
+            return Fibonacci_recursive(n - 1) + Fibonacci_recursive(n - 2);
         }
+    }
+
+    public static int Fibonacci_loop(int n) {
+        if (n <= 2) {
+            return 1;
+        }
+        int result = 0;
+        int lastNumber1 = 1;
+        int lastNumber2 = 1;
+        for (int i = 2; i < n; i++) {
+            result = lastNumber1 + lastNumber2;
+            lastNumber1 = lastNumber2;
+            lastNumber2 = result;
+        }
+        return result;
     }
 }
 
