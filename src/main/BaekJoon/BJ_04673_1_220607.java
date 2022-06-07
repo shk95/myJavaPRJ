0d816306bea00cc233620e358b7d1505e9810d7c
user123145 <shk9510@gmail.com>
2022-06-07 23:22:45 +0900

    /22.06.07 /백준문제 /해결 완료. 코드를 더 단순화시킬수 있을지 모르겠다.

--- src/main/BaekJoon/BJ_04673_1_220607.java
+++ src/main/BaekJoon/BJ_04673_1_220607.java
@@ -1,11 +1,14 @@
 public class BJ_04673_1_220607 {
     public static void main(String[] args) {
+        /*셀프넘버를 1~10000까지 구함. 셀프 넘버가 아닌것을 배열 인덱스
+        0~10000번에 카운트, 카운트가 안된 배열의 인덱스를 출력하는 방식으로
+        값 구함.     */
         int[] numArr = new int[10001];
-        int x = 0;
+        int num = 0;
         for (int i = 1; i < 10001; i++) {
-            x = functionD(i, digitNum(i));
-            if (x < 10001) {
-                numArr[x] = 1;
+            num = functionD(i, digitNum(i));
+            if (num < 10001) {
+                numArr[num] = 1;
             }
         }
        for (int i = 1; i < 10001; i++) {
@@ -22,7 +25,7 @@ public class BJ_04673_1_220607 {
         int result = 0;
         int num = 0, point = 1;
         for (int i = 1; i <= n2; i++) {
-            num = n1 / point;
+            num = (n1/point)%10;
             point *= 10;
             result += num;
         }
