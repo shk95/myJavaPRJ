0ea3c191995630e1fbbe878be860c16721ac5a96
user123145 <shk9510@gmail.com>
2022-06-08 21:26:43 +0900

    /22.06.08 /백준 /'한 수' 구하기 알고리즘 작성중

--- src/main/BaekJoon/BJ_01065_1_220608.java
+++ src/main/BaekJoon/BJ_01065_1_220608.java
@@ -13,11 +13,27 @@ public class BJ_01065_1_220608 {
     }
 
 }
+/*1부터 N 까지의 '한 수' 의 개수를 구하는 함수.
+한수 : 자릿수별 숫자를 수열의 각각의 항이라 했을때, 그 수열이 등차수열을 이룬다.*/
 class Function{
-    public int num(int n) {
-
-        int result = 0;
-
-        return result;
+    public int num(int N) {
+        int count = 0;
+        int index; //자릿수 개수
+        int x; //나누는수
+        int num;//구해지는 수
+        for (int i = 1; i <= N; i++) {
+            if (i < 100) {
+                count+=1;
+            }else{
+                x = 1;
+                index = 1;
+                num = 0;
+                while (i/x!=0) {
+                    num = (i/x)%10;
+                    x*=10;
+                }
+            }
+        }
+        return count;
     }
 }
