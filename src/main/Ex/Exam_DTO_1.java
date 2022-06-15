2020f353417624e364cfa2ae3f30fb7a88e3a047
user123145 <shk9510@gmail.com>
2022-06-15 11:02:30 +0900

    /수업 /getter setter

---
+++ src/main/Ex/Exam_DTO_1.java
@@ -0,0 +1,22 @@
+//getter setter
+//private 를 외부에서 접근하기위한 함수
+public class Exam_DTO_1 {
+    private String test;
+    private int score;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;//객체의 변수를 가리킴
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
