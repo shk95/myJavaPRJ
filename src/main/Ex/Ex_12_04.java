1071d86b0f9597181d420913f032e153a90651ca
user123145 <shk9510@gmail.com>
2022-06-21 14:22:31 +0900

    /학습 /getter setter 추가해보기

--- src/main/Ex/Ex_12_04.java
+++ src/main/Ex/Ex_12_04.java
@@ -1,6 +1,13 @@
 class Car_6 {
     private String color;
     int speed;
+
+    public void setColorMethod(String color){
+       this.color = color;
+    }
+    public String getColorMethod() {
+        return color;
+    }
 }
 
 class Sedan_c extends Car_6 {
@@ -22,5 +29,7 @@ public class Ex_12_04 {
         sedan1.setSpeed(300);
 //        speed 필드에 접근 가능
         System.out.println("승용차 속도 = " + sedan1.speed);
+        sedan1.setColorMethod("빨강");
+        System.out.println("승용차 색깔 = "+sedan1.getColorMethod());
     }
 }
