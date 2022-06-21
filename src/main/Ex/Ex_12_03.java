c11c402259989c127ff103bd58249814d169e7da
user123145 <shk9510@gmail.com>
2022-06-21 14:04:17 +0900

    /학습 /super() 메소드 사용 추가. 수퍼클래스의 생성자를 super() 로 링크를 걸어주는 느낌

--- src/main/Ex/Ex_12_03.java
+++ src/main/Ex/Ex_12_03.java
@@ -8,15 +8,30 @@ class Car_b {
 }
 
 class Sedan_b extends Car_b {
+    //생성자의 순서 영향받는다.
+    Sedan_b(){
+        //슈퍼클래스메소드생성자
+        super("super() 메소드");
+        System.out.println("noParam");
+    }
     Sedan_b(String str) {
+        super();
         System.out.println("서브 클래스 생성자 호출 ==>"+str);
+
     }
 }
 
 public class Ex_12_03 {
 
     public static void main(String[] args) {
+
         Sedan_b sedan_1 = new Sedan_b("생성자 문자열 파라미터");
+        System.out.println("-------- ");
+        Sedan_b sedan_2 = new Sedan_b();
+        System.out.println("ㄴ 파라미터 없는 서브클래스의 생성자");
+        System.out.println("-------- ");
+
+        Car_b super_1 = new Car_b("슈퍼클래스 생성자 : 파라미터");
 
     }
 }
